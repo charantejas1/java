f371cebc9616cfad0a05d24a267e5631a6ed1162
charan <206639905+charantejas1@users.noreply.github.com>
2026-08-17 15:42:26 +0530

    java

---
+++ program 1.java
@@ -0,0 +1,13 @@
+public class variableDemo {
+    public  static void main(String[] args) {
+        int rollNo = 101;               
+        double marks= 92.5;   
+        char section = 'D';         
+        boolean ispass = marks >= 40;       // Boolean
+        String name = "charan";     // String
+
+        System.out.println(name + "(" + rollNo + ")");
+        System.out.println("marks : " + marks);
+        System.out.println("pass : " + ispass);
+    }
+    
